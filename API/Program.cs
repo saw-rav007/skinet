60c2e6abdca7e49244126234405f4d64ff7d9d9d
Saurav Ranjan <s.k.ranjan1997@gmail.com>
2021-05-29 00:58:27 +0530

    Added the seed data using the json files

--- API/Program.cs
+++ API/Program.cs
@@ -24,6 +24,7 @@ namespace API
                 {
                     var context = services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
                 }
                 catch (Exception ex)
                 {
